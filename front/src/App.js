1784cec035af22d6fd453f0539a91f675cab2aec
IrinaK <s8obod@mail.ru>
2021-12-10 10:04:44 +0300

    profile

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,9 @@ import Header from './components/Header/Header'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
 import Mainpage from './components/Mainpage/Mainpage'
+import Profile from './components/Prifile/Profile'
+import Logout from './components/Logout/Logout'
+
 import './App.css'
 
 function App() {
@@ -14,6 +17,9 @@ function App() {
       <Route path='/login' element={ <Login />}/>
       <Route path='/register' element={ <Register />}/>
       <Route path='/mainpage' element={ <Mainpage />}/>
+      <Route path='/profile' element={ <Profile />}/>
+      <Route path='/logout' element={ <Logout />}/>
+
     </Routes>
     </main>
     </>
--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -10,10 +10,11 @@ function Header() {
         
         <Link className={styles.head_links} to='/mainpage'><i className="fas fa-home"></i> Home</Link>
         <Link className={styles.head_links} to='/'><i className="fas fa-map-marker-alt"></i> Places</Link>
-        <Link className={styles.head_links} to='/'><i className="fas fa-user"></i> Profile</Link>
+        <Link className={styles.head_links} to='/profile'><i className="fas fa-user"></i> Profile</Link>
         <Link className={styles.head_links} to='/login'> <i className="fas fa-sign-in-alt"></i> Login</Link>
-        <Link className={styles.head_links} to='/register'> <i className="fas fa-sign-in-alt"></i> Register</Link>
-
+        <Link className={styles.head_links} to='/logout'> <i className="fas fa-sign-out-alt"></i> Logout</Link>
+        <Link className={styles.head_links} to='/register'> <i className="fas fa-clipboard-check"></i> Register</Link>
+      
       </div>
 
       <div className={`${styles.header_bottom_container} container`}> 
