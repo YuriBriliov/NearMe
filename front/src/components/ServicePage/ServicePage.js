b9a69da0c512dcf223f8fc991b5e9501c4b86c82
Yuri Briliov <yuribriliov@gmail.com>
2021-12-10 16:20:42 +0300

    Release day 2 evening

--- front/src/components/ServicePage/ServicePage.js
+++ front/src/components/ServicePage/ServicePage.js
@@ -9,18 +9,38 @@ function ServicePage() {
     return state.cards
   })
 
+  let arrCards = [
+    {
+      id: 1,
+      image: 'https://i.ibb.co/GFcfRrK/Intersect.png',
+      title: 'title',
+      category: 'category',
+      price: 'price'
+    },
+    {
+      id: 2,
+      image: 'https://i.ibb.co/GFcfRrK/Intersect.png',
+      title: 'title',
+      category: 'category',
+      price: 'price'
+    },
+    {
+      id: 3,
+      image: 'https://i.ibb.co/GFcfRrK/Intersect.png',
+      title: 'title',
+      category: 'category',
+      price: 'price'
+    },
+  ]
+
 
   return (
     <>
     <h3 className={classes.title__block}>Title block</h3>
     <section className={classes.cards}>
-        {cards.map((item)=>{
+        {arrCards.map((item)=>{
           return <Card key={item.id} {...item} />
         })}
-{/*       
-      <Card />
-      <Card />
-      <Card /> */}
     </section>
     </>
   )
