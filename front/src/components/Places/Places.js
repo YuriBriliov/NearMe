6d4f3a853aa5370639492374c27438572941490b
Yuri Briliov <yuribriliov@gmail.com>
2021-12-13 15:25:47 +0300

    Release refuck

--- front/src/components/Places/Places.js
+++ front/src/components/Places/Places.js
@@ -1,23 +1,33 @@
+import { useEffect } from 'react'
 import styles from './places.module.css'
 import Card from '../Card/Card'
 import { useThemeContext } from '../../context/themeContext'
 import MapsTest from '../MapsTest/MapsTest'
 import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 function Places() {
   const { isLightTheme , setTheme} = useThemeContext()
-
+  const navigate = useNavigate()
   const cards = useSelector(state => {
     return state.cards
   })
 
   function detailOnMap(event) {
     event.preventDefault()
-    console.log(event.target)
+    if (event.target.tagName === 'BUTTON') {
+      navigate(`/card/${event.target.dataset.id}`)
+    }
   }
 
-  let cardOnMap = document.querySelector(`${styles.places_mapbox_light}`)
-  // console.log(cardOnMap)
+
+  let cardOnMap
+  useEffect(()=>{
+    cardOnMap = document.querySelector(`.${styles.places_mapbox_light}`)
+    cardOnMap.addEventListener('click', detailOnMap)
+  },[])
+  
+  
 
   return (
     <>
@@ -27,20 +37,18 @@ function Places() {
         тут будут фильтры
       </div>
       <div className={`${styles.cards_block_light} scroll_light`}>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+            {cards.map((item)=>{
+              return <Card key={item.id} {...item} />
+            })}
+      
+
       </div>
     </div>
 
       <div className={styles.places_mapbox_light}>
         <MapsTest cards={cards} />
       </div>
-      </div>}
+    </div>}
 
       {!isLightTheme && <div className={styles.places_main_container_dark}>
     <div className={styles.places_sidebar_dark}>
@@ -49,12 +57,7 @@ function Places() {
       </div>
       <div className={`${styles.cards_block_dark} scroll_dark`}>
       <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+
       </div>
     </div>
 
