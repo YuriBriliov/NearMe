4d3a2745a4754b59b9df09fd78b83e599504653e
Yuri Briliov <35831590+YuriBriliov@users.noreply.github.com>
2021-12-20 17:17:54 +0300

    Delete front directory

--- front/src/redux/reducers/root.reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-// import { validationReducer } from './validation.reducer'
-import { userReducer } from './user.reducer'
-
-
-export const rootReducer = combineReducers({
-  // validation: validationReducer,
-  user: userReducer
-})
--- front/src/redux/types.js
+++
@@ -1,9 +0,0 @@
-// types
-
-// user
-export const REGISTER_USER = 'REGISTER_USER'
-export const SET_USER = 'SET_USER'
-export const USER_LOGOUT = 'USER_LOGOUT'
-
-export const CHECK_USER = 'CHECK_USER'
-export const SET_USER_ERROR = 'SET_USER_ERROR'
